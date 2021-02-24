191322fab801fea0e2e237ec2c0a39fa22cf4548
Tan Dang <tandm160797@gmail.com>
2021-02-24 22:00:35 +0700

    Add Post API - TanDM

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import postController from '../core/controllers/PostController';
+import { auth } from './../middlewares';
 
 const router = Router();
 
+router.use(auth);
 router.get('/', postController.index);
 router.post('/', postController.create);
 router.get('/:id', postController.show);
